f6f52f457c8b0d63357c071b82e22ea1e007cfa6
daianasg1 <daianasoledadg@hotmail.com>
2023-01-11 09:16:26 -0300

    crear estados, variables, editar img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+   {/* Creamos una variable linea1, y una funcion setLinea1 que permite modificar el valor de la variable */}
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+ setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+     }
+
+     const onChangeImagen = function(evento){
+      setImagen(evento.target.value)
+       }
+
+       const onClickExportar = function(evento){
+     alert("EXPORTAR")
+         }
+
   return (
     <div className="App">
      {/* Select picker de memes*/}
-      <select>
+      <select onChange={onChangeImagen}>
         <option value="fire">Casa en llamas</option>
         <option value="futurama">Futurama</option>
         <option value="history">History Channel</option>
@@ -13,17 +35,21 @@ function App() {
         <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart Guy</option>
         
-      </select>
+      </select> <br/>
 
 
-      {/* Input text - primer linea*/}
-      <input type="text" />
-
-       {/* Input text- Segunda linea*/}
-       <input type="text" />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1"/> <br/>
+       <input onChange={onChangeLinea2} type="text" placeholder="Linea 2"/> <br/>
 
       {/* Boton exportar */}
-      <button>Exportar</button>
+      <button onClick={onClickExportar}>Exportar</button>
+
+      {/*estructura del meme*/}
+      <div className="meme">
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span><br/>
+        <img src={"/img/" + imagen + ".jpg"}/>
+      </div>
 
 
     </div>
